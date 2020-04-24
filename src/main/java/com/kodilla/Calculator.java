d0f62612c1ee3c86762b37cb6a8fe5c044d855cb
hubertb98 <hubertb.98.hb@gmail.com>
2020-04-25 00:26:11 +0200

    Calculator app

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -1,5 +1,21 @@
 package com.kodilla;
 
 public class Calculator {
+    double a = 17;
+    double b = 3.2;
 
+    public double add() {
+        return a + b;
+    }
+    public double substract() {
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        calc.add();
+        calc.substract();
+        System.out.println(calc.add());
+        System.out.println(calc.substract());
+    }
 }
